07a38901d4753b14ff55cc5cf06e2233046b6bb8
sharmayogesh152004@gmail.com <sharmayogesh152004@gmail.com>
2025-09-05 15:03:25 +0530

    random

---
+++ 04_random_password/index.js
@@ -0,0 +1,37 @@
+const passwordBox = document.getElementById("password");
+
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const lowwerCase = "abcdefghijklmnopqrstuvwxyz";
+
+const number = "0123456789"
+
+const symbol = "!@#$%^&*()_{}[]><";
+
+const allChars = upperCase + lowwerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+
+    password += lowwerCase[Math.floor(Math.random() * lowwerCase.length)];
+
+    password += number[Math.floor(Math.random() * number.length)];
+
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length ){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+}
+
+
+function copyPassword(){
+  passwordBox.select();
+  document.execCommand("copy");
+}
