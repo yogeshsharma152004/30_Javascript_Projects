355d9546e261049cdda27626a95075c61e867519
sharmayogesh152004@gmail.com <sharmayogesh152004@gmail.com>
2025-09-08 13:42:01 +0530

    password

---
+++ 19_Password_Strength/index.js
@@ -0,0 +1,30 @@
+var pass = document.getElementById("password");
+
+var msg = document.getElementById("message");
+
+var str = document.getElementById("strenght");
+
+pass.addEventListener('input' , () => {
+    if(pass.value.length > 0){
+       msg.style.display = "block";
+    }
+    else{
+        msg.style.display = "none";
+    }
+
+    if(pass.value.length < 4){
+        str.innerHTML = "Weak";
+        pass.style.borderColor = "#ff5925"
+        msg.style.color = "#ff5925"
+    }
+    else if(pass.value.length >= 4 && pass.value.length < 8){
+          str.innerHTML = "Mediam";
+          pass.style.borderColor = "yellow"
+        msg.style.color = "yellow"
+    }
+    else if(pass.value.length >= 8){
+         str.innerHTML = "Strong";
+         pass.style.borderColor = "#26d730";
+        msg.style.color = "#26d730"
+    }
+})
